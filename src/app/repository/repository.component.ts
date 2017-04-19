9f8e7e19a4d408f9aa9f359d7435be70436d3f49
fisheax <fisheax@gmail.com>
2017-04-19 10:38:38 +0800

    integrate the git-webui

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-repository',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject("windowObject") private window: Window) { }
 
   ngOnInit() {
+    if (!(<any>window).mainui) {
+      (<any>window).mainui = new (<any>window).MainUi();
+    }
   }
 
 }
